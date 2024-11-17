5a9c8bbc63254b70621ab888539be1df90917abb
Tanat <ttangun1@gmail.com>
2024-11-17 20:33:27 +0700

    feat: finished first draft

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use async_trait::async_trait;
+use pingora::{
+    prelude::HttpPeer,
+    proxy::{http_proxy_service, ProxyHttp, Session},
+    server::Server,
+    Result,
+};
+
+pub struct Gateway;
+
+impl Gateway {
+    pub fn new() -> Self {
+        Gateway {}
+    }
+}
+
+#[async_trait]
+impl ProxyHttp for Gateway {
+    type CTX = ();
+
+    fn new_ctx(&self) -> Self::CTX {
+        ()
+    }
+
+    async fn upstream_peer(&self, _session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
+        Ok(Box::new(HttpPeer::new(
+            "one.one.one.one:443".to_string(),
+            true,
+            "one.one.one.one".to_string(),
+        )))
+    }
+}
+
+fn main() {
+    let mut server = Server::new(None).expect("[main] failed to start pingora server");
+    server.bootstrap();
+
+    let mut service = http_proxy_service(&server.configuration, Gateway::new());
+
+    service
+        .add_tls("0.0.0.0:4430", "./keys/one_cert.crt", "./keys/one_key.pem") // TODO change this to config
+        .expect("[main] failed to add tls config");
+
+    server.add_service(service);
+    server.run_forever()
+}
